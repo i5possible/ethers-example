c23299dc3636a411c6d8b74c25f0d93a8ee571cc
Hong Liang <lianghongbuaa@gmail.com>
2022-01-14 10:26:54 +0800

    add the example

--- README.md
+++ README.md
@@ -1,70 +1,22 @@
-# Getting Started with Create React App
+# Ethers-example
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is an example of a project using [Ethers](https://docs.ethers.io/v5/getting-started/) to interact with the Ethereum blockchain.
 
-## Available Scripts
+We integrate the Ethers with MetaMask and trying to connect/disconnect to the wallet and change the account.
+We are able to get the active address and get the balance.
 
-In the project directory, you can run:
+We used [ganache-cli](https://github.com/celo-org/ganache-cli) to create a test Ethereum blockchain to test.
+You could install it using `npm i -g ganache-cli && ganache-cli`.
+See [MetaMask](https://docs.metamask.io/guide/getting-started.html#basic-considerations) for more information.
 
-### `npm start`
+## Run
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Start a test network using ganache-cli.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Run `yarn install && yarn start` to start this project.
 
-### `npm test`
+Connect the wallet using Localhost:8545 at MetaMask. Restore the account to the wallet.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Known issues
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Failed when we are trying to transfer to another account.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,137 @@
-import logo from './logo.svg';
+import { useState, useEffect, useCallback } from 'react';
+import { ethers } from 'ethers';
 import './App.css';
 
 function App() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [account, setAccount] = useState('');
+  const [sendTo, setSendTo] = useState('');
+  const [balance, setBalance] = useState('');
+  const [provider, setProvider] = useState();
+
+  const updateWalletInfo = useCallback(async () => {
+    try {
+      const provider = await new ethers.providers.Web3Provider(window.ethereum);
+      const address = await provider.getSigner().getAddress();
+      const bigNumber = await provider.getBalance(address);
+      console.log(provider);
+      console.log(address);
+      console.log(bigNumber);
+      setProvider(provider);
+      setAccount(address);
+      setBalance(ethers.utils.formatUnits(bigNumber, 'ether'));
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const handleDisconnectWallet = useCallback(async () => {
+    setProvider(null);
+    setAccount('');
+    setBalance('');
+    setWalletConnected(false);
+  }, []);
+
+  const handleChainChanged = useCallback(async (chainId) => {
+    window.location.reload();
+  }, []);
+
+  const handleAccountsChanged = useCallback(
+    (accounts) => {
+      if (accounts.length === 0) {
+        console.log('Please connect to MetaMask.');
+      } else if (accounts[0] !== account) {
+        setAccount(accounts[0]);
+        setWalletConnected(true);
+      }
+    },
+    [account]
+  );
+
+  // isConnected? ethereum, provider : null
+  useEffect(() => {
+    setWalletConnected(window.ethereum && window.ethereum.selectedAddress);
+  }, []);
+
+  useEffect(() => {
+    if (walletConnected) {
+      updateWalletInfo();
+    }
+  }, [walletConnected, updateWalletInfo]);
+
+  useEffect(() => {
+    if (!window.ethereum) {
+      return;
+    }
+    window.ethereum.on('accountsChanged', updateWalletInfo);
+    window.ethereum.on('disconnect', handleDisconnectWallet);
+    window.ethereum.on('chainChanged', handleChainChanged);
+  }, [updateWalletInfo, handleDisconnectWallet, handleChainChanged]);
+
+  useEffect(() => {
+    if (!provider) {
+      return;
+    }
+    // Force page refreshes on network changes
+    provider.on('network', (newNetwork, oldNetwork) => {
+      // When a Provider makes its initial connection, it emits a "network"
+      // event with a null oldNetwork along with the newNetwork. So, if the
+      // oldNetwork exists, it represents a changing network
+      if (oldNetwork) {
+        window.location.reload();
+      }
+    });
+  }, [provider]);
+
+  const handleConnect = () => {
+    // use eth_requestAccounts to get the accounts
+    window.ethereum
+      .request({ method: 'eth_requestAccounts' })
+      .then(handleAccountsChanged)
+      .catch(() => {
+        console.log('Failed to connect to wallet!');
+      });
+  };
+
+  const handleOnChange = (e) => {
+    setSendTo(e.target.value);
+  };
+
+  const handleOnSend = async () => {
+    const amount = ethers.utils.parseEther('1');
+    const signer = provider.getSigner();
+    console.log(`signerAddress`, await signer.getAddress());
+    window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [{ from: account, to: sendTo, value: amount.toHexString() }],
+    });
+    // This is eth_sendRawTransaction which is not supported by the test chain
+    // signer.sendTransaction({
+    //   to: sendTo,
+    //   value: amount,
+    // });
+  };
+
+  console.log(account);
+  console.log(balance);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body">
+        <p>Connect to Ethereum and show the wallet info</p>
+        <button className="btn" onClick={handleConnect}>
+          Connect to wallet
+        </button>
+        {walletConnected && <p>Account: {account}</p>}
+        {walletConnected && <p>Balance: {balance} ETH</p>}
+        <input
+          className="input"
+          value={sendTo}
+          onChange={handleOnChange}></input>
+        <button className="btn" onClick={handleOnSend}>
+          Send 1 ETH
+        </button>
+      </div>
     </div>
   );
 }
